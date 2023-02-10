e1f06d1b176277874ce009106008ff5c7ba5377d
onuraydinnn <onuraydin27@hotmail.com>
2023-02-10 13:53:26 +0300

    Odev Son

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Listele(List<Musteri> listMusteri)
+        {
+            foreach (var list in listMusteri) 
+            {
+                Console.WriteLine(list.Id+" "+list.Ad+" "+list.MusteriNo);
+            }
+        }
+        public void Ekle(Musteri musteri, List<Musteri> listMusteri)
+        {
+            listMusteri.Add(musteri);
+            Console.WriteLine(musteri.Ad + " eklendi");
+        }
+        public void Sil(Musteri musteri, List<Musteri> listMusteri)
+        {
+            listMusteri.Remove(musteri);
+            Console.WriteLine(musteri.Ad + " silindi");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -10,6 +10,24 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
+            Musteri musteri1= new Musteri() {Id=1, Ad="Onur", MusteriNo="12345"};
+            Musteri musteri2 = new Musteri() { Id = 2, Ad = "Ahmet", MusteriNo = "83702" };
+            Musteri musteri3 = new Musteri() { Id = 3, Ad = "Mehmet", MusteriNo = "23836" };
+            Musteri musteri4 = new Musteri() { Id = 4, Ad = "Ayse", MusteriNo = "28128" };
+            Musteri musteri5 = new Musteri() { Id = 5, Ad = "Fatma", MusteriNo = "35125" };
+
+            List<Musteri> listMusteri = new List<Musteri>();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri4, listMusteri);
+            musteriManager.Ekle(musteri5, listMusteri);
+            musteriManager.Listele(listMusteri);
+            musteriManager.Sil(musteri4, listMusteri);
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
